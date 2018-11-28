2f0a12401246510cb046a888118a076400396541
fuzhengxin <fuzhengxin@baidu.com>
2018-11-28 19:26:04 +0800

    20181128 基础框架提交

---
+++ src/main/java/com/fsindustry/debussy/metaserver/App.java
@@ -0,0 +1,17 @@
+package com.fsindustry.debussy.metaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 程序入口
+ *
+ * @author fuzhengxin
+ */
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
